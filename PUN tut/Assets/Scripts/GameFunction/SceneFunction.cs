d0d48db35bce423de2ccbccab80bd8eafa75c236
CqH <chenqiuhong45@gmail.com>
2021-06-16 01:55:48 +0800

    UI demo update

---
+++ PUN tut/Assets/Scripts/GameFunction/SceneFunction.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Com.MyCompany.MyGame
+{
+    public class SceneFunction : MonoBehaviour
+    {
+        #region Public Methods
+        public void JumpToScene_Single(string index)
+        {
+            SceneManager.LoadScene(index,LoadSceneMode.Single);
+        }
+        public void JumpToScene_Additive(string index)
+        {
+            SceneManager.LoadScene(index, LoadSceneMode.Additive);
+        }
+        public void Debug_Log(string message)
+        {
+            Debug.Log(message);
+        }
+
+        #endregion
+    }
+}
