7c15860610a5375d60b82858ba26ce6984162673
Timothy197 <1195796206@qq.com>
2021-06-30 17:18:01 +0800

    board update
    1. board can be preserved in the scene
    2. the board has a time limit to be preserved for 1 day (according to utc time)

---
+++ PUN tut/Assets/BoardChat/Scripts/BoardManager.cs
@@ -0,0 +1,89 @@
+﻿// this script should be attached to the board prefab to control the behavior of the board
+// the behavior of the board: 1. destroy itself when it has been placed for 1 day
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+namespace Com.MyCompany.MyGame
+{
+    public class BoardManager : MonoBehaviourPun
+    {
+        private System.DateTime utcDate;
+        private int[] initDate;
+
+        private void Start()
+        {
+            PhotonView PV = transform.GetComponent<PhotonView>(); // get the photon view of the board
+            initDate = (int[])PV.InstantiationData[1];
+
+        }
+
+        private void FixedUpdate() // check if the board has reached the time limit
+        {
+            utcDate = System.DateTime.Now; // get the time now
+            int[] nowDate = { utcDate.Year, utcDate.Month, utcDate.Day, utcDate.Hour, utcDate.Minute };
+
+            if (CheckTerminate(initDate, nowDate))
+            {
+                // destroy the board
+                photonView.RPC("TerminateBoard", RpcTarget.MasterClient);
+            } // else preserve the board in the scene
+        }
+
+        /// <summary>
+        /// the function is to check if the interval between 2 time is greater than one day
+        /// </summary>
+        /// <param name="initDate">the before time</param>
+        /// <param name="nowDate">the after time</param>
+        /// <returns></returns>
+        private bool CheckTerminate(int[] initDate, int[] nowDate)
+        {
+            if (nowDate[0] == initDate[0] && nowDate[1] == initDate[1] && nowDate[2] == initDate[2]) // when on the same day
+            {
+                if (nowDate[3] * 60 + nowDate[4] >= initDate[3] * 60 + nowDate[4]) return false; // do not destroy
+                else return true;
+            }
+            else if (nowDate[0] > initDate[0]) // year different
+            {
+                if (initDate[1] == 12 && nowDate[1] == 1 && initDate[2] == 31 && nowDate[2] == 1 &&
+                    ((24 - initDate[3] + nowDate[3]) < 24 || (((24 - initDate[3] + nowDate[3]) == 24) && nowDate[4] < initDate[4])))
+                    return false; // do not destroy
+                else return true;
+            }
+            else if (nowDate[1] > initDate[1]) // month different
+            {
+                if (nowDate[1] - initDate[1] == 1)
+                {
+                    if (nowDate[1] == 1)
+                    {
+                        if (initDate[1] == 1 || initDate[1] == 3 || initDate[1] == 5 || initDate[1] == 7 || initDate[1] == 8 || initDate[1] == 10 || initDate[1] == 12)
+                        {
+                            if (((24 - initDate[3] + nowDate[3]) < 24 || (((24 - initDate[3] + nowDate[3]) == 24) && nowDate[4] < initDate[4]))) return false;
+                            else return true;
+                        }
+                        else
+                        {
+                            if (((24 - initDate[3] + nowDate[3]) < 24 || (((24 - initDate[3] + nowDate[3]) == 24) && nowDate[4] < initDate[4]))) return false;
+                            else return true;
+                        }
+                    }
+                    else return true;
+                }
+                else return false;
+            }
+            else if (nowDate[2] > initDate[2]) //day different
+            {
+                if (((24 - initDate[3] + nowDate[3]) < 24 || ((initDate[3] == nowDate[3]) && nowDate[4] < initDate[4]))) return false; // less than one day
+                else return true;
+            }
+            else return true;
+        }
+
+        [PunRPC]
+        void TerminateBoard()
+        {
+            PhotonNetwork.Destroy(transform.GetComponent<PhotonView>());
+        }
+    }
+}
--- PUN tut/Assets/BoardChat/Scripts/PlayerSetBoard.cs
+++ PUN tut/Assets/BoardChat/Scripts/PlayerSetBoard.cs
@@ -3,43 +3,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 
 namespace Com.MyCompany.MyGame
 {
     public class PlayerSetBoard : MonoBehaviourPun
     {
-
-        /// <summary>
-        /// to transfer the ownership to set the board as a room object
-        /// </summary>
-        private void onSetBoard()
+        private System.DateTime utcDate;
+        private void Start()
         {
-            base.photonView.RequestOwnership();
+
         }
 
-        
+
         public void placeBoard(string input)
         {
-            Debug.Log("the photon ID of the current local player is:" + PhotonNetwork.LocalPlayer.ActorNumber);
-            Debug.Log("the master client of the room is: " + PhotonNetwork.NetworkingClient.CurrentRoom.MasterClientId);
             
             Quaternion rotation = PlayerManager.LocalPlayerInstance.transform.rotation;
             Vector3 position = PlayerManager.LocalPlayerInstance.transform.position;
+            utcDate = System.DateTime.UtcNow;
 
-            object[] instanceData = new object[1];
+            object[] instanceData = new object[2];
             instanceData[0] = input;
-            Debug.Log("check, and now the isMasterClient is" + PhotonNetwork.IsMasterClient);
+            int[] dateInfo = {utcDate.Year, utcDate.Month, utcDate.Day, utcDate.Hour, utcDate.Minute};
+            instanceData[1] = dateInfo;
             if (!(PhotonNetwork.IsMasterClient)) 
             {
-                Debug.Log("the transfer is successful?: " + PhotonNetwork.SetMasterClient(PhotonNetwork.LocalPlayer));
-                Debug.Log("change, and the local player now is: " + PhotonNetwork.LocalPlayer.ActorNumber);
-                Debug.Log("change, and the master client now is: " + PhotonNetwork.NetworkingClient.CurrentRoom.MasterClientId);
-            } 
-            
-            PhotonNetwork.InstantiateRoomObject("Board1", position + rotation * (new Vector3(0f, 0f, 2f)), rotation, 0, instanceData);
+                photonView.RPC("masterClientInstantiate", RpcTarget.MasterClient, position, rotation, instanceData);
+            } else 
+                PhotonNetwork.InstantiateRoomObject("Board1", position + rotation * (new Vector3(0f, 0f, 2f)), rotation, 0, instanceData);
 
-            Debug.Log("the Board text is:" + instanceData[0]);
-            Debug.Log("the board should be set");
+            // below is for set
+            //Debug.Log("the Board text is:" + instanceData[0]);
+            //Debug.Log("the set board time is: " + ((int[])instanceData[1])[3]);
+            //Debug.Log("the board should be set");
+        }
+
+        [PunRPC]
+        void masterClientInstantiate(Vector3 pos, Quaternion rot, object[] pass_data)
+        {
+            PhotonNetwork.InstantiateRoomObject("Board1", pos + rot * (new Vector3(0f, 0f, 2f)), rot, 0, pass_data);
         }
         
     }
