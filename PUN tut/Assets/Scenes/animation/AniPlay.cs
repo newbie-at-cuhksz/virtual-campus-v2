ba302f011decd96b0ea0fb4e9d6073978ed89c9e
CqH <chenqiuhong45@gmail.com>
2021-07-06 21:13:37 +0800

    camera main menu

---
+++ PUN tut/Assets/Scenes/animation/AniPlay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AniPlay : MonoBehaviour
+{
+    private GameObject go;
+    int clipCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        go = this.gameObject;
+        clipCount = go.GetComponent<Animation>().GetClipCount();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PUN tut/Assets/Scenes/animation/CameraMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CamAnimation
+{
+
+    public class CameraMove : MonoBehaviour
+    {
+        // Start is called before the first frame update
+
+        public float speed=10;
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            transform.position += input * (speed * Time.deltaTime);
+        }
+    }
+
+}
