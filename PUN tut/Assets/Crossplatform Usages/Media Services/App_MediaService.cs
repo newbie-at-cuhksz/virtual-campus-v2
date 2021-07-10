1577364c2e0980526d02fb5fc9ebe4366492b84d
Timothy197 <1195796206@qq.com>
2021-07-10 22:42:26 +0800

    modify mediaService and add a asset to support multiple images import

--- PUN tut/Assets/Crossplatform Usages/Media Services/App_MediaService.cs
+++ PUN tut/Assets/Crossplatform Usages/Media Services/App_MediaService.cs
@@ -9,47 +9,59 @@ public class App_MediaService : MonoBehaviour
     GalleryAccessStatus readAccessStatus;
     GalleryAccessStatus readWriteAccessStatus;
     CameraAccessStatus cameraAccessStatus;
-    public Texture2D currentImage; //IG_MediaService.instance.currentImage;
+    public Texture2D currentImage; //App_MediaService.instance.currentImage;
+    public List<Texture2D> currentImages; // App_MediaService.instance.currentImages; to select more than one image
+
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
+        currentImages = new List<Texture2D>();
     }
-    //App_MediaService.instance.GetImageFromGallery();
-    public void GetImageFromGallery()
+
+    //App_MediaService.instance.GetImageFromGallery(maxSize);
+    public void GetImageFromGallery(int maxSize)
     {
-        Debug.Log("the get image method invoked");
-        readAccessStatus = MediaServices.GetGalleryAccessStatus(GalleryAccessMode.Read);
-        if (!(readAccessStatus == GalleryAccessStatus.Authorized))
+        NativeGallery.Permission permission = NativeGallery.GetImageFromGallery((path) =>
         {
-            MediaServices.RequestGalleryAccess(GalleryAccessMode.Read, callback: (result, error) =>
+            Debug.Log("Image path: " + path);
+            if (path != null)
             {
-                Debug.Log("Request for gallery access finished.");
-                Debug.Log("Gallery access status: " + result.AccessStatus);
-            });
-        }
-        if (readAccessStatus == GalleryAccessStatus.Authorized)
-        {
-            MediaServices.SelectImageFromGallery(canEdit: true, (textureData, error) =>
-            {
-                if (error == null)
+                // Create Texture from selected image
+                currentImage = NativeGallery.LoadImageAtPath(path, maxSize);
+                // Event is triggered here
+                if (currentImage == null)
                 {
-                    Debug.Log("Select image from gallery finished successfully.");
-                    //textureData.GetTexture() // This returns the texture
-                    currentImage = textureData.GetTexture();
-                    //The event of successufully get the image goes here
+                    Debug.Log("Couldn't load texture from " + path);
+                    return;
                 }
-                else
-                {
-                    Debug.Log("Select image from gallery failed with error. Error: " + error);
-                }
-            });
-
-        }
+            }
+        });
 
     }
-    //App_MediaService.instance.TakePictureWithCamera();
-    public void TakePictureWithCamera()
+
+    public void GetImagesFromGallery(int maxSize) // get more than one image, size is how many images you want to fetch
+    {
+        //currentImages = new List<Texture2D>(); // renew the instance
+        NativeGallery.Permission permission = NativeGallery.GetImagesFromGallery((paths) =>
+        {
+            foreach (var path in paths)
+            {
+                Debug.Log("Image path: " + path);
+                if (path != null)
+                {
+                    // Create Texture from selected image
+                    currentImages.Add(NativeGallery.LoadImageAtPath(path, maxSize));
+                    Debug.Log("in process the images size is: " + currentImages.Count);
+                }
+            }
+            Debug.Log("the images size is: " + currentImages.Count);
+            //Event is triggered here
+        });
+    }
+
+        //App_MediaService.instance.TakePictureWithCamera();
+        public void TakePictureWithCamera()
     {
         cameraAccessStatus = MediaServices.GetCameraAccessStatus();
         if (cameraAccessStatus != CameraAccessStatus.Authorized)
